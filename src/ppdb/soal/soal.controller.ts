6bb2ada509d6d520bb2e20c103c997cdab447977
Radistian <radistian.azka@gmail.com>
2025-05-30 09:52:44 +0700

    update soal

--- src/ppdb/soal/soal.controller.ts
+++ src/ppdb/soal/soal.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Post, Body, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body, Delete, Param, Put } from '@nestjs/common';
 import { SoalService } from './soal.service';
 import { Soal } from 'src/ppdb/entity/soal.entity';
 
@@ -16,9 +16,19 @@ export class SoalController {
   createSoal(@Body() soal: Partial<Soal>){
     return this.soalService.create(soal);
   }
+
+  @Put('update/:id')
+  updateSoal(@Param('id') id: string, @Body() soal: Partial<Soal>) {
+    return this.soalService.updateSoal(id, soal);
+  }
   
   @Delete('delete')
   deleteSoal(@Query('id') id: string) {
     return this.soalService.deleteSoal(id);
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string) {
+    return this.soalService.findById(id);
   }
 }
--- src/ppdb/soal/soal.module.ts
+++ src/ppdb/soal/soal.module.ts
@@ -3,9 +3,10 @@ import { SoalService } from './soal.service';
 import { SoalController } from './soal.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Soal } from 'src/ppdb/entity/soal.entity';
+import { OpsiJawaban } from '../entity/opsiJawaban.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Soal])],
+  imports:[TypeOrmModule.forFeature([Soal, OpsiJawaban])],
   providers: [SoalService],
   controllers: [SoalController]
 })
